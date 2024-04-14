ed31e845b211e559209dd6fe61605a5133612899
Satyam1Vishwakarma <97113954+Satyam1Vishwakarma@users.noreply.github.com>
2024-04-14 22:53:53 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use api_video::rapi;
 
 #[tokio::main]
 async fn main(){
-    let a=rapi::ApiVideo{production:false,token:"GymsBb4Vf2o7C8SCbCUiRBd8125Xv9wP5M8CK9y4hXh".to_string()};
+    let a=rapi::ApiVideo{production:false,token:"GymsBb4VP5M8CK9y4hXh".to_string()};
     a.video_upload("title".to_string(), "G:\\Rust\\python\\fo.mp4".to_string()).await;
 }
