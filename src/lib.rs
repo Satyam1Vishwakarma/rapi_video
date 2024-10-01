4a12d11d7f1e3392e13d1738d80797d74a0e42ad
Satyam1Vishwakarma <97113954+Satyam1Vishwakarma@users.noreply.github.com>
2024-10-01 13:13:43 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -143,3 +143,140 @@ pub mod rapi{
     }
 
 }
+
+
+pub mod rapi_sync{
+
+    use reqwest::blocking::multipart;
+    use reqwest::StatusCode;
+    use serde_json::{self, json};
+    use std::fs::File;
+    
+    #[warn(dead_code)]
+    pub struct ApiVideo {
+        pub production: bool,
+        pub token: String,
+    }
+    
+    #[warn(dead_code)]
+    struct Uri {
+        pro: String,
+        sand: String,
+    }
+    
+    #[warn(dead_code)]
+    impl ApiVideo {
+        fn geturl(&self) -> String {
+            let z = Uri {
+                pro: "https://ws.api.video".to_string(),
+                sand: "https://sandbox.api.video".to_string(),
+            };
+    
+            let z = match self.production {
+                true => z.pro,
+                false => z.sand,
+            };
+            return z;
+        }
+    
+        fn video_object(
+            &self,
+            url: String,
+            public: bool,
+            panoramic: bool,
+            mp4support: bool,
+            _title: String,
+        ) -> String {
+            let z =
+                json!({"public":public,"panoramic":panoramic,"mp4Support":mp4support,"title":"title"});
+    
+            let client = reqwest::blocking::Client::new();
+            let res: serde_json::Value = client
+                .post(url)
+                .bearer_auth(&self.token)
+                .header("Content-Type", "application/json")
+                .json(&z)
+                .send()
+                .unwrap()
+                .json()
+                .unwrap();
+    
+            let mut vid = String::new();
+            for a in res["videoId"].to_string().chars() {
+                if a != '"' {
+                    vid.push(a);
+                }
+            }
+            return vid;
+        }
+    
+        ///Upload video to api.video
+        /// # Example
+        /// video_upload("myfirstvideo","G:\video.mp4")
+    
+        pub fn video_upload(&self, title: String, full_file_path: String) -> String {
+            let z = &self.geturl();
+    
+            let url = format!("{}/videos", z);
+    
+            let vid = match self.production {
+                true => self.video_object(url, true, false, true, title),
+                false => self.video_object(url, true, false, true, title),
+            };
+    
+            let metadata = std::fs::metadata(full_file_path.clone()).unwrap();
+            let file_len = metadata.len();
+    
+            if file_len < 209715200 {
+                let file = File::open(full_file_path).unwrap();
+                let part = multipart::Part::reader(file);
+    
+                let form = multipart::Form::new()
+                .part("file", part);
+    
+                let url = format!("{}/videos/{}/source", z, vid);
+                let client = reqwest::blocking::Client::new();
+                client
+                    .post(url)
+                    .bearer_auth(&self.token)
+                    .multipart(form)
+                    .send()
+                    .unwrap();
+            }
+    
+            let z = format!("https://embed.api.video/vod/{}", vid);
+            return z;
+        }
+    
+        pub fn get_all_video(&self, page_num: u8, page_size: u8) -> serde_json::Value {
+            let z = &self.geturl();
+            let url = format!(
+                "{}/videos?currentPage={}&pageSize={}'",
+                z, page_num, page_size
+            );
+            let client = reqwest::blocking::Client::new();
+            let res: serde_json::Value = client
+                .get(url)
+                .bearer_auth(&self.token)
+                .header("accept", "application/json")
+                .send()
+                .unwrap()
+                .json()
+                .unwrap();
+    
+            return res;
+        }
+    
+        pub fn del_video(&self, vid: String) -> StatusCode {
+            let url = format!("https://ws.api.video/videos/{}", vid);
+            let client = reqwest::blocking::Client::new();
+            let res = client
+                .delete(url)
+                .bearer_auth(&self.token)
+                .send()
+                .unwrap();
+            return res.status();
+        }
+    }
+
+}
